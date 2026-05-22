c6b042c8045f42f721c86c0a01bc71193bd7f98e
Robert James Cabansay <rjamscxx@gmail.com>
2026-05-23 02:07:03 +0800

    feat: Day 11 change styles with JavaScript.

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let heading = document.getElementById("main-title");
 
-heading.textContent = "Hello from Javascript!";
+document.getElementById("main-title").style.color = "dodgerblue";
+
+heading.textContent = "Hello from JavasSript!";
 
 let paragraph = document.getElementById("description");
 
+document.getElementById("description").stylefontSize = "20px";
+
 paragraph.textContent = "Robert is learning DOM manipulation";
+
+document.body.style.backgroundColor = "#f0f0f0";
